f40ffb7e83969783d5c7afad009f0b2fee2f5d3e
Yves Accad <yaccad@gmail.com>
2021-09-13 22:24:56 -0700

    fix ami and org

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Accadius"
 
     workspaces {
       name = "gh-actions-demo"
@@ -30,7 +30,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-0747bdcabd34c712a"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 

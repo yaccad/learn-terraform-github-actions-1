2c211a9a2e1e1ed5255845ff0b0d97b330bd6d23
Yves Accad <yaccad@gmail.com>
2021-09-13 21:10:37 -0700

    fix ami ref

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-0e011417bd70948da"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
